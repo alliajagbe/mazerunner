93f8cf49270d77cb708cbfa6e4d3541d8e618cb6
alliajagbe <alliajagbe001@gmail.com>
2023-09-22 23:27:36 +0530

    updated function

--- mazerunner_dfs.py
+++ mazerunner_dfs.py
@@ -13,9 +13,9 @@ def moveGen(current_state):
         new_row, new_col = row + dr, col + dc
 
     if (
-        0 <= new_row < len(current_state.maze) 
-        and 0 <= new_col < len(current_state.maze[0])
-        and current_state.maze[new_row][new_col] == 0
+        0 <= new_row < len(current_state.maze) # check if new_row is within the maze
+        and 0 <= new_col < len(current_state.maze[0]) # check if new_col is within the maze
+        and current_state.maze[new_row][new_col] == 0 # check if the new position is not a wall (0 for path, 1 for wall)
     ):
         new_position = (new_row, new_col)
         neighbours.append(MazeState(current_state.maze, new_position))
