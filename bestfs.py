f73e9e859916cb3be9dc3615767f724bea462acb
alliajagbe <alliajagbe001@gmail.com>
2023-09-23 19:45:48 +0530

    heuristic function

--- bestfs.py
+++ bestfs.py
@@ -1,4 +1,11 @@
 import mazerunner
 
+
+def heuristic(state, goal_position):
+    row_distance = abs(state.position[0] - goal_position[0])
+    col_distance = abs(state.position[1] - goal_position[1])
+    distance = row_distance + col_distance
+    return distance
+
 def best_first_search(initial_state, goal_position):
     pass
