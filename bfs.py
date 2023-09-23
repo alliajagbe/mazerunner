883415d73851efb82717b474a1af37ceb2c39483
alliajagbe <alliajagbe001@gmail.com>
2023-09-23 19:20:11 +0530

    breadth first search function

---
+++ bfs.py
@@ -0,0 +1,18 @@
+import mazerunner
+
+def breadth_first_search(initial_state, goal_position):
+    queue = [(initial_state, [])]
+    explored = set()
+
+    while queue:
+        current_state, path = queue.pop(0)
+        explored.add(current_state.position)
+
+        if mazerunner.goalTest(current_state, goal_position):
+            return path + [current_state.position]
+        
+        for neighbour in mazerunner.moveGen(current_state):
+            if neighbour.position not in explored:
+                queue.append((neighbour, path + [current_state.position]))
+
+    return None
