a644aaefe5badcb04f866f0e876c303d6d615ac4
alliajagbe <alliajagbe001@gmail.com>
2023-09-23 19:58:32 +0530

    best first search function

--- bestfs.py
+++ bestfs.py
@@ -8,4 +8,20 @@ def heuristic(state, goal_position):
     return distance
 
 def best_first_search(initial_state, goal_position):
-    pass
+    priority_queue = [(initial_state, [], 0)]
+    explored = set()
+
+    while priority_queue:
+        current_state, path, cost = priority_queue.pop(0)
+        explored.add(current_state.position)
+
+        if mazerunner.goalTest(current_state, goal_position):
+            return path + [current_state.position]
+        
+        for neighbour in mazerunner.moveGen(current_state):
+            if neighbour.position not in explored:
+                priority_queue.append((neighbour, path + [current_state.position], cost + heuristic(neighbour, goal_position)))
+                priority_queue.sort(key=lambda x: x[2])
+
+    return None
+
