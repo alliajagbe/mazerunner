833f7b0647285449a75c3ef03d688d8e4ed70f88
alliajagbe <alliajagbe001@gmail.com>
2023-09-24 14:39:00 +0530

    average of 10 runs with formatted path output

--- test.py
+++ test.py
@@ -28,29 +28,64 @@ def main():
     # taking an average of 10 runs
     time_dfs = []
     states_dfs = []
-    for i in range(10):
-        start_time = time.perf_counter()
-        dfs_solution, no_of_states_explored = depth_first_search(initial_state, goal_position)
-        end_time = time.perf_counter()
-        time_dfs.append(end_time - start_time)
-        states_dfs.append(no_of_states_explored)
 
+    time_bfs = []
+    states_bfs = []
+
+    time_bestfs = []
+    states_bestfs = []
+    for i in range(10):
+        start_time_dfs = time.perf_counter()
+        dfs_solution, no_of_states_explored_dfs = depth_first_search(initial_state, goal_position)
+        end_time_dfs = time.perf_counter()
+        time_dfs.append(end_time_dfs - start_time_dfs)
+        states_dfs.append(no_of_states_explored_dfs)
+
+        start_time_bfs = time.perf_counter()
+        bfs_solution, no_of_states_explored_bfs = breadth_first_search(initial_state, goal_position)
+        end_time_bfs = time.perf_counter()
+        time_bfs.append(end_time_bfs - start_time_bfs)
+        states_bfs.append(no_of_states_explored_bfs)
+
+        start_time_bestfs = time.perf_counter()
+        bestfs_solution, no_of_states_explored_bestfs = best_first_search(initial_state, goal_position)
+        end_time_bestfs = time.perf_counter()
+        time_bestfs.append(end_time_bestfs - start_time_bestfs)
+        states_bestfs.append(no_of_states_explored_bestfs)
+
+    
     print("Average time for DFS:", sum(time_dfs)/len(time_dfs))
     print("Average number of states explored with DFS:", sum(states_dfs)/len(states_dfs))
-    print("Path found with DFS:", dfs_solution)
+
+    if dfs_solution:
+        for i in range(len(dfs_solution)):
+            print(dfs_solution[i], end=" ")
+        print()
+    else:
+        print("No path found with DFS")
+
+    print("Average time for BFS:", sum(time_bfs)/len(time_bfs))
+    print("Average number of states explored with BFS:", sum(states_bfs)/len(states_bfs))
+    
+    if bfs_solution:
+        for i in range(len(bfs_solution)):
+            print(bfs_solution[i], end=" ")
+        print()
+    else:
+        print("No path found with BFS")
+
+    print("Average time for BestFS:", sum(time_bestfs)/len(time_bestfs))
+    print("Average number of states explored with BestFS:", sum(states_bestfs)/len(states_bestfs))
+    
+    if bestfs_solution:
+        for i in range(len(bestfs_solution)):
+            print(bestfs_solution[i], end=" ")
+        print()
+    else:
+        print("No path found with BestFS")
 
 
-    start_time = time.perf_counter()
-    bfs_solution, no_of_states_explored = breadth_first_search(initial_state, goal_position)
-    end_time = time.perf_counter()
-    print("Number of states explored with BFS:", no_of_states_explored)
-    print("BFS took", end_time - start_time, "seconds")
 
-    start_time = time.perf_counter()
-    bestfs_solution, no_of_states_explored = best_first_search(initial_state, goal_position)
-    end_time = time.perf_counter()
-    print("Number of states explored with BestFS:", no_of_states_explored)
-    print("BestFS took", end_time - start_time, "seconds")
 
 
     if dfs_solution:
