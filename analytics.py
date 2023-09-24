497d77a35f85c73fc6939d219853523e17c4f8b6
alliajagbe <alliajagbe001@gmail.com>
2023-09-24 16:38:13 +0530

    time visualizer

--- analytics.py
+++ analytics.py
@@ -1,10 +1,20 @@
 import psutil
 import matplotlib.pyplot as plt
+from test import main
 
 def get_memory_usage():
     process = psutil.Process()
     memory_info = process.memory_info()
     return memory_info.rss
 
-def no_of_steps_visualizer():
-    pass
+def visualizer():
+    avg_time_dfs, avg_states_dfs, avg_memory_dfs, avg_time_bfs, avg_states_bfs, avg_memory_bfs, avg_time_bestfs, avg_states_bestfs, avg_memory_bestfs, dfs_solution, bfs_solution, bestfs_solution = main()
+
+    # Plotting time
+    time = [avg_time_dfs, avg_time_bfs, avg_time_bestfs]
+    labels = ["DFS", "BFS", "BestFS"]
+    plt.bar(labels, time)
+    plt.title("Average time for each algorithm")
+    plt.xlabel("Algorithm")
+    plt.ylabel("Time (s)")
+    plt.show()
